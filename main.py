6288efee322dfd7523e3a787d1eedcd473294e77
Léo Dubus <dubusster@gmail.com>
2017-07-07 00:05:36 +0200

    finished get_number

--- main.py
+++ main.py
@@ -5,27 +5,33 @@ Created on 6 juil. 2017
 '''
 
 LETTER_DICT = {
-    'abc':2,
-    'def':3,
-    'ghi':4,
-    'jkl':5,
-    'mno':6,
-    'pqrs':7,
-    'tuv':8,
-    'wxyz':9,
-    ' ': 0
+    'abc2':2,
+    'def3':3,
+    'ghi4':4,
+    'jkl5':5,
+    'mno6':6,
+    'pqrs7':7,
+    'tuv8':8,
+    'wxyz9':9,
+    ' 0': 0
     }
 
-def get_number():
-    pass
-
-if __name__ == '__main__':
-    inp = 'hey'
+def get_number(inp):
+    inp = str(inp)
     res = 0
     for c in inp:
         for key in LETTER_DICT.keys():
             if c in key:
-                idx = key.index(c) + 1
-                res += LETTER_DICT[key]
+                idx = key.index(c) + 2
+                res += idx * LETTER_DICT[key]
+    return res
+
+def get_inp(url):
+    pass
+
+
+if __name__ == '__main__':
+    inp = '29c9f7c4b35e2213079440ab776f57db'
+    res = get_number(inp)
 
     print(res)
