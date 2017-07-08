e19f1493947eab56f5ad2afc7b3133a99f1e4a10
Léo Dubus <dubusster@gmail.com>
2017-07-09 01:06:22 +0200

    working but long

--- 20.py
+++ 20.py
@@ -12,15 +12,15 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def find_answer(words):
-    for word1 in words:
+def find_answer(word1, words):
 
-        for word2 in words:
+    for word2 in words:
 
-            w = word1 + word2
-            md5_hex = hashlib.md5(str.encode(w))
-            if md5_hex.hexdigest() == answer:
-                return w
+        w = word1 + word2
+        md5_hex = hashlib.md5(str.encode(w))
+        if md5_hex.hexdigest() == answer:
+            print("Found ! ==> %s" % w)
+            return w
 
 if __name__ == '__main__':
     answer = "2e52b110695075c2c733e2f0ee69f59f"
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     futures = []
     with ProcessPoolExecutor(20) as executor:
 
-
-        for chunk in chunks(words, int(len(words) / 10)):
-            futures.append(executor.submit(find_answer, chunk))
+        for word1 in tqdm(words):
+            for chunk in chunks(words, 50000):
+                futures.append(executor.submit(find_answer, word1, chunk))
 
         for x in as_completed(futures):
             if x.result() is not None:
