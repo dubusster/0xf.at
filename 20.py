c4f48971fb3752e62d7ef544c3a866d94ee971cc
Léo Dubus <dubusster@gmail.com>
2017-07-09 12:16:59 +0200

    creates a rule for hashcat

--- 20.py
+++ 20.py
@@ -5,7 +5,8 @@ Created on 9 juil. 2017
 '''
 import hashlib
 from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed, ThreadPoolExecutor
+import gc
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
@@ -26,18 +27,41 @@ if __name__ == '__main__':
     answer = "2e52b110695075c2c733e2f0ee69f59f"
     f = "wordlist.txt"
     with open(f, 'r') as fid:
-        words = fid.read()
+        words = fid.readlines()
 
-    futures = []
-    with ProcessPoolExecutor(20) as executor:
+    with open("words.rule", 'w') as rule_list:
+        for word in words:
+            word = word.strip()
+            for c in word:
+                rule_list.write('$')
+                rule_list.write(c)
+                rule_list.write(' ')
+            rule_list.write('\n')
 
-        for word1 in tqdm(words):
-            for chunk in chunks(words, 50000):
-                futures.append(executor.submit(find_answer, word1, chunk))
 
-        for x in as_completed(futures):
-            if x.result() is not None:
-                print(x.result())
+#     futures = []
+#     with ProcessPoolExecutor(20) as executor:
+#
+#         for word1 in tqdm(words):
+#             for chunk in chunks(words, 50000):
+#                 futures.append(executor.submit(find_answer, word1, chunk))
+#             if len(futures) >= 10000:
+#                 print("checking done futures")
+#                 for future in tqdm(as_completed(futures)):
+#                     res = future.result()
+#                     if res is not None:
+#                         print(res)
+#                         exit(0)
+#                 print("\n\n\nflushing done futures")
+#                 futures = []
+#                 gc.collect()
+#
+#
+#         print("finished submitting")
+
+
+
+
 
 
 
